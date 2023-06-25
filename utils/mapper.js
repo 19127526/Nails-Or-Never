2908faa6b46b3bcc9b86da13ee8dd7b932c4be4e
hoangphuc552001 <hoangphuc552001@gmail.com>
2023-06-25 18:16:28 +0700

    additional

--- utils/mapper.js
+++ utils/mapper.js
@@ -3,6 +3,16 @@ function userResponse(user) {
     return {id, username, email, role, token}
 }
 
+function parseLimit(limit) {
+    return parseInt(limit) || 5
+}
+
+function parsePage(page) {
+    return parseInt(page) || 1
+}
+
 module.exports = {
-    userResponse
+    userResponse,
+    parseLimit,
+    parsePage
 }
