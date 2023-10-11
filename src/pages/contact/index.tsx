0b93521df1764a5f73c08a87eaeea2b9355f3207
Your Name <phamtienquan2001@gmail.com>
2023-10-11 17:18:12 +0700

    update

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -147,7 +147,7 @@ const ContactPage = (props: any) => {
                         <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
                         <meta name="viewport" content="initial-scale=1, width=device-width"/>
                         <meta name="robots" content="index,follow"/>
-                        <link ref="canonical" href="http://nailsornever.com"/>
+                        <link ref="canonical" href="https://nailsornever.com"/>
 
                         <meta name="description" content={` Located conveniently in Malta, NewYork, 12118,
                         ${process.env.NEXT_PUBLIC_NAME_PRODUCT} is one of the best salons in this area. ${process.env.NEXT_PUBLIC_NAME_PRODUCT} offers premier nails care and spa treatment services to satisfy your needs of enhancing natural beauty and refreshing your day.
@@ -157,7 +157,7 @@ const ContactPage = (props: any) => {
                         You always feel friendly and welcome from our passionate staff who understand what you want.`}/>
                         <meta name="keywords"
                               content={`${process.env.NEXT_PUBLIC_NAME_PRODUCT} &amp; SPA,MALTA,Contact extensions`}/>
-                        <meta property="og:url" content="http://nailsornever.com/"/>
+                        <meta property="og:url" content="https://nailsornever.com/"/>
                         <meta property="og:type" content="Website"/>
                         <meta property="og:title"
                               content={`Contact with me - ${process.env.NEXT_PUBLIC_NAME_PRODUCT}`}/>
--- src/pages/gift-card/index.tsx
+++ src/pages/gift-card/index.tsx
@@ -39,7 +39,7 @@ const GiftCardPage = (props : any) => {
                 <meta httpEquiv="X-UA-Compatible"content="IE=edge"/>
                 <meta name="viewport" content="initial-scale=1, width=device-width"/>
                 <meta name="robots" content="index,follow"/>
-                  <link ref="canonical" href="http://nailsornever.com"/>
+                  <link ref="canonical" href="https://nailsornever.com"/>
 
                 <meta name="description" content={`Located conveniently in Malta, NewYork, 12118,
                         ${process.env.NEXT_PUBLIC_NAME_PRODUCT} is one of the best salons in this area. ${process.env.NEXT_PUBLIC_NAME_PRODUCT} offers premier nails care and spa treatment services to satisfy your needs of enhancing natural beauty and refreshing your day.
@@ -48,7 +48,7 @@ const GiftCardPage = (props : any) => {
                         Let your sweetheart know how much you love and care for him/her by sending our love cards! Buy our gift card for your loved one.`}/>
                 <meta name="keywords"
                       content={`${process.env.NEXT_PUBLIC_NAME_PRODUCT} &amp; SPA,MALTA,GiftCard extensions`}/>
-                <meta property="og:url" content="http://nailsornever.com/"/>
+                <meta property="og:url" content="https://nailsornever.com/"/>
                 <meta property="og:type" content="Website"/>
                 <meta property="og:title" content={`Gift card - ${process.env.NEXT_PUBLIC_NAME_PRODUCT}`}/>
                 <meta property="og:description" content={`Located conveniently in Malta, NewYork, 12118,
